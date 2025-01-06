ba298330663dbcc996dcdac79f780c2595547727
baijanaththaru-skillprompt <baijanaththaru@skillprompt.com>
2025-01-06 16:37:28 +0545

    migration for note table

---
+++ src/migrations/note-table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS notes
+(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  description TEXT,
+  priority INT,
+  created_at TIMESTAMP DEFAULT NOW()
+);
